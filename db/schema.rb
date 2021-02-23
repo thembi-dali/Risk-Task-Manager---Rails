7d36e610924456f47eed3480b0bce39fbde9a727
Thembi Baleni <thembi.baleni@gmail.com>
2021-02-23 17:18:21 +1100

    Rails basic task maanager

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_004834) do
+ActiveRecord::Schema.define(version: 2021_02_23_010721) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.integer "rating"
+    t.string "title"
+    t.text "detail"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
